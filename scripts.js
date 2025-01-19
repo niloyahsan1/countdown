1f9953278c7456d34e45303ef16659e3f73a498b
Niloy Ahsan <157811017+niloyahsan1@users.noreply.github.com>
2025-01-19 19:38:17 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,32 +1,5 @@
 // change the date for countdown
-const countdownDate1 = new Date("February 10, 2024 02:00:00").getTime();
-
-const x1 = setInterval(function () {
-  const now = new Date().getTime();
-
-  const distance = countdownDate1 - now;
-
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById(
-    "countdown1"
-  ).innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-
-  if (distance < 0) {
-    clearInterval(x1);
-    document.getElementById("countdown1").innerHTML = "Countdown is OVER!";
-  }
-}, 1000);
-
-// ===================================================================================
-
-// change the date for countdown
-const countdownDate2 = new Date("February 11, 2024 23:00:00").getTime();
+const countdownDate2 = new Date("January 27, 2024 23:00:00").getTime();
 
 const x2 = setInterval(function () {
   const now = new Date().getTime();
@@ -47,33 +20,6 @@ const x2 = setInterval(function () {
   if (distance < 0) {
     clearInterval(x2);
     document.getElementById("countdown2").innerHTML = "Countdown is OVER!";
-  }
-}, 1000);
-
-// ===================================================================================
-
-// change the date for countdown
-const countdownDate3 = new Date("February 11, 2024 23:00:00").getTime();
-
-const x3 = setInterval(function () {
-  const now = new Date().getTime();
-
-  const distance = countdownDate3 - now;
-
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById(
-    "countdown3"
-  ).innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-
-  if (distance < 0) {
-    clearInterval(x3);
-    document.getElementById("countdown3").innerHTML = "Countdown is OVER!";
   }
 }, 1000);
 
